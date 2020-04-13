9507a6692f244a3ff21fd3a096433a058c5e6bd2
mtouhidchowdhury <mtc405@nyu.edu>
2020-04-13 13:45:54 -0400

    add contact-us component
    added contact-us component with the contact us form

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './components/admin-login/admin-login.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminLoginComponent
+    AdminLoginComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule
