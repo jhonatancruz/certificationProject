68017a3822f1b6a2d98c1143572935968c97b5f6
Farhan Hyder <fmshyder@gmail.com>
2020-04-13 19:21:44 -0400

    removed previous admin-login component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AdminLoginComponent } from './components/admin-login/admin-login.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
@@ -10,7 +9,6 @@ import { FooterComponent } from './components/shared/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AdminLoginComponent,
     ContactUsComponent,
     HeaderComponent,
     FooterComponent
