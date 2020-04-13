2852de658cc1b3809175ab8d75f567e966072898
acarm1 <ramazanmansur@gmail.com>
2020-04-13 15:18:49 -0400

    added user service made changes on admin login and user class

--- front-end/src/app/components/admin-login/admin-login.component.ts
+++ front-end/src/app/components/admin-login/admin-login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Users } from 'src/app/models/user/users'
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
+
+
 
 @Component({
   selector: 'app-admin-login',
@@ -7,9 +13,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLoginComponent implements OnInit {
 
-  constructor() { }
+  userName: string = '';
+  password: string = '';
+users: Observable<Users[]>;
+  
+constructor(private router: Router, private user : UserService )
+ { }
 
   ngOnInit(): void {
   }
+
+
+  handleSubmit() {
+    // this.users =  this.user.getUsers(); // IDK what to do after here
+      if (this.userName === 'admin' && this.password === 'admin') {
+        //navigate to tasks.
+        this.router.navigate(['/home']);
+      }else if
+      (this.userName === 'user' && this.password === 'user') {
+        //navigate to tasks.
+        this.router.navigate(['/home-user']);
+    }
 
 }
--- front-end/src/app/models/user/users.ts
+++ front-end/src/app/models/user/users.ts
@@ -1,2 +1,20 @@
 export class Users {
+
+    id: number;
+    userName: string;
+    email: string;
+    password: string;
+    role: string;
+    completed: boolean;
+    date: Date;
+  
+    constructor(id: number, userName: string, role: string,email = '',password: string, completed = false, date = new Date()) {
+      this.id = id;
+      this.userName = userName;
+      this.email = email;
+      this.password = password;
+      this.role=role;
+      this.completed = completed;
+      this.date = date;
+    }
 }
