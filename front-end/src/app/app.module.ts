a7b26cc3ce3739ed9dff5a95182572c4e34105ff
acarm1 <ramazanmansur@gmail.com>
2020-04-13 14:07:55 -0400

    added header and footer

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './components/admin-login/admin-login.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminLoginComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
