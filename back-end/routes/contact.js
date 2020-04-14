534e51f1b31bb8987e8515191de77e12d996cf8e
Jhonatan Cruz <jcruz3@drew.edu>
2020-04-13 20:34:49 -0400

    put sender email in a variable

--- back-end/routes/contact.js
+++ back-end/routes/contact.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const sgMail = require('@sendgrid/mail');
 
 const apiKey = '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' // should be store in env variable
+const senderVerifiedEmail= 'jcruz3@drew.edu'
 
 // sends email
 router.post('/', (req, res) => {
@@ -10,7 +11,7 @@ router.post('/', (req, res) => {
     sgMail.setApiKey(apiKey)
     const msg = {
         to: to,
-        from: 'jcruz3@drew.edu',
+        from: senderVerifiedEmail,
         subject: subject,
         text: message
         // html: '<strong>and easy to do anywhere, even with Node.js</strong>',
