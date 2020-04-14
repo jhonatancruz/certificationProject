de78b688b9ffcac8877f7ad599a5ae2dca69fdd6
mtouhidchowdhury <mtc405@nyu.edu>
2020-04-14 13:34:54 -0400

    updated contact us
    contact us send email

--- front-end/src/app/components/contact-us/contact-us.component.ts
+++ front-end/src/app/components/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactusService } from 'src/app/services/contactus.service';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,8 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
+  contact_email: string = '';
+  contact_name: string = '';
+  contact_message: string = '';
+  constructor(private contactservice: ContactusService) { }
 
-  constructor() { }
+  handleSubmit(){
+    const data = {
+      name: this.contact_name,
+      email: this.contact_email,
+      message: this.contact_message
+    };
+
+    this.contactservice.sendEmail(data).subscribe(() => {
+      console.log('Sent email')
+    })
+
+    this.contact_email ='';
+    this.contact_message = '';
+    this.contact_name = '';
+  }
 
   ngOnInit(): void {
   }
