88ebe1330fce3c7af4ee3aa3ca795e5e5205ff51
Jhonatan Cruz <jcruz3@drew.edu>
2020-04-14 00:49:23 -0400

    completely redeigned api in order to prevent blank document bug

--- back-end/routes/news.js
+++ back-end/routes/news.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const axios = require('axios').default;
-
 
 const News = require('../models/News')
 const auth = require('../middlewares/auth')
+const newsApi = require('../middlewares/newsApi')
 
 // Create news
-router.post('/',auth, (req, res) => {
+router.post('/', auth, (req, res) => {
     const { sourceName, author, title, description, type, url, urlToImage, content } = req.body
 
     const news = new News()
@@ -62,7 +61,7 @@ router.put('/:id', auth, (req, res) => {
 
 
 //delete news by id
-router.delete('/:id',auth, (req, res) => {
+router.delete('/:id', auth, (req, res) => {
     const { id } = req.params
 
     News.findByIdAndRemove(id)
@@ -70,66 +69,46 @@ router.delete('/:id',auth, (req, res) => {
         .catch(err => res.status(400).json(err))
 })
 
-
-// Get news by query word
-router.get('/query', auth,(req, res) => {
-    const date = new Date()
-    const formattedDate = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate()
-    const apiKey = 'e105798436064d8eb738c72a933fa352'
-    const { q } = req.query
-
-    axios.get('https://newsapi.org/v2/everything?q=' + q + '&from=' + formattedDate + '&sortBy=publishedAt&apiKey=' + apiKey)
-        .then(function (result) {
-            res.send(result.data.articles)
-        })
-        .catch(function (error) {
-            res.send(error)
-        })
+// Get news based on query you provide
+router.get('/query', auth, async (req, res) => {
+    const { query } = req.body
+    newsApi.query(query).then(result => {
+        News.insertMany(result)
+            .then(result => {
+                res.json(result)
+            })
+            .catch(err => {
+                res.status(400).json(err)
+            })
+    })
 })
 
-// Get news by category
-router.get('/breaking', auth,(req, res) => {
-    const apiKey = 'e105798436064d8eb738c72a933fa352'
-    const { category } = req.query
-
-    axios.get('https://newsapi.org/v2/top-headlines?country=us&category=' + category + '&apiKey=' + apiKey)
-        .then(function (result) {
-            var allNews = []
-            for (i = 0; i < result.data.articles.length; i++) {
-                const sourceName = result.data.articles[i].source.name
-                const author = result.data.articles[i].author
-                const title = result.data.articles[i].title
-                const description = result.data.articles[i].description
-                const url = result.data.articles[i].url
-                const urlToImage = result.data.articles[i].urlToImage
-                const publishedAt = result.data.articles[i].publishedAt
-                const content = result.data.articles[i].content
-
-                const news = new News()
-                news.sourceName = sourceName
-                news.author = author
-                news.title = title
-                news.description = description
-                news.type = category
-                news.url = url
-                news.urlToImage = urlToImage
-                news.publishedAt = publishedAt
-                news.content = content
-
-                allNews.push(news)
-            }
-            News.insertMany(allNews)
-                .then(newNews => {
-                    res.send('Added news to database')
-                })
-                .catch(err => {
-                    res.status(400).json(err)
-                })
-        })
-        .catch(function (error) {
-            res.send(error)
-        })
-
-
+// Get headline breaking news
+router.get('/breaking', auth, async (req, res) => {
+    newsApi.getTopNews().then(result => {
+        News.insertMany(result)
+            .then(result => {
+                res.json(result)
+            })
+            .catch(err => {
+                res.status(400).json(err)
+            })
+    })
 })
+
+// Get headline sport news
+router.get('/sports', auth, async (req, res) => {
+    newsApi.getSportNews().then(result => {
+        News.insertMany(result)
+            .then(result => {
+                res.json(result)
+            })
+            .catch(err => {
+                res.status(400).json(err)
+            })
+    })
+})
+
+
+
 module.exports = router;
