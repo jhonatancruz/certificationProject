1f6b88e06cda911b9d084e4a42ce030d9559b070
Farhan Hyder <fmshyder@gmail.com>
2020-04-13 22:42:25 -0400

    admin login logout done

--- front-end/src/app/components/admin/admin-header/admin-header.component.ts
+++ front-end/src/app/components/admin/admin-header/admin-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-header',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private loginService : LoginService,
+    private router : Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  handleLogOut(){
+    console.log('inside handle logout');
+    this.loginService.logout();
+    this.router.navigate(['admin/login'])
+    
   }
 
 }
