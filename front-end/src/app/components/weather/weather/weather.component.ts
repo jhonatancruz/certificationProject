b179b8da2ec435c1ee8a207d085a6736779cdeb6
Farhan Hyder <fmshyder@gmail.com>
2020-04-15 01:21:39 -0400

    updated weather component

--- front-end/src/app/components/weather/weather/weather.component.ts
+++ front-end/src/app/components/weather/weather/weather.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from 'src/app/services/weather/weather.service';
+import { LocationService } from 'src/app/services/location/location.service';
 
 @Component({
   selector: 'app-weather',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherComponent implements OnInit {
 
-  constructor() { }
+  lat : string = '40.712776';
+  lon : string = '-74.005974';
+
+  weatherReport : any = {};
+
+  constructor(
+    private weatherService : WeatherService,
+    private locationService : LocationService
+  ) { }
 
   ngOnInit(): void {
+    this.loadWeatherReport()
   }
+
+  getLocation()
+  {
+    this.locationService.getPosition()
+    .then(pos=>
+      {
+         console.log(`Positon: ${pos.lon} ${pos.lat}`);
+
+         this.lat = pos.lat;
+         this.lon = pos.lon;
+      })
+      .then(() => {
+        this.loadWeatherReport()
+      })
+  }
+
+  loadWeatherReport(){
+    let location = {
+      lat : this.lat,
+      lon : this.lon
+    }
+
+    this.weatherService.getWeather(location)
+    .subscribe(result =>{
+      console.log(result);
+      
+    })
+
+  }
+
+
 
 }
