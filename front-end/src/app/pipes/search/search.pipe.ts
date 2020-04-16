b5fb1e2ce01237e3851f6121d3b400083c7b3f76
Farhan Hyder <fmshyder@gmail.com>
2020-04-16 16:29:13 -0400

    bug fixed: for search on empty description

--- front-end/src/app/pipes/search/search.pipe.ts
+++ front-end/src/app/pipes/search/search.pipe.ts
@@ -8,7 +8,13 @@ export class SearchPipe implements PipeTransform {
   transform(value: any, ...args: any[]): any {
 
     return value.filter((item) => {
-      return (item.title.toUpperCase().indexOf(args[0].toUpperCase()) >= 0) || 
+
+      if(item.description === null){
+        return ((item.title.toUpperCase().indexOf(args[0].toUpperCase()) >= 0)) || 
+            (item.type.toUpperCase().indexOf(args[0].toUpperCase()) >= 0);
+      }
+
+      return ((item.title.toUpperCase().indexOf(args[0].toUpperCase()) >= 0)) || 
             (item.description.toUpperCase().indexOf(args[0].toUpperCase()) >= 0) ||
             (item.type.toUpperCase().indexOf(args[0].toUpperCase()) >= 0);
     });
